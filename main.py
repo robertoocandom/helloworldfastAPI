9efe3fc24d75168d1c9817f843b341592a5e0128
Robert Ocando <rocando@jpglobaldigital.com>
2022-05-21 12:41:05 -0500

    cookie_and_Header

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 #Python
+from ast import For
 from doctest import Example
 from email import message
+from tkinter.messagebox import NO
 from typing import Optional
 from enum import Enum
 
 #Pydantic
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from pydantic import Field
+from pydantic import EmailStr
 
 #FastAPI
-from fastapi import FastAPI, Query
+from fastapi import Cookie, FastAPI, Header, Query
 from fastapi import status
 from fastapi import Body, Query, Path, Form
 
@@ -163,6 +166,8 @@ def update_person(
     return results
 
 
+### Forms
+
 @app.post(
     path="/login",
     response_model=LoginOut,
@@ -170,3 +175,31 @@ def update_person(
 )
 def login(username: str = Form(...,), password: str = Form(...)):
     return LoginOut(username=username)
+
+
+### Cookies and Headers Paraneters #### 
+
+@app.post(
+    path='/contact',
+    status_code=status.HTTP_200_OK
+)
+def contact(
+    first_name:str = Form(
+        ...,
+        max_leght=20,
+        min_length=1
+    ),
+    last_name:str = Form(
+        ...,
+        max_leght=20,
+        min_length=1
+    ),
+    email: EmailStr = Form(...),
+    message: str = Form(..., 
+    min_length=20
+    ),
+    user_agent: Optional[str] = Header(default=None),
+    ads: Optional[str] = Cookie(default=None)
+):
+    return user_agent
+
